cc955b7c31ea9e4726bc7792c8b960e31b5e9cf9
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2016-09-15 09:32:59 -0400

    Use basic fitness function to sort species by fitness

--- main.py
+++ main.py
@@ -3,9 +3,26 @@ import numpy as np
 INITIAL_SPECIES_SIZE = 500
 WEIGHTS_PER_MOBILE = 8
 
+TOO_MANY_LAYERS_FACTOR = 3.0
+DUPLICATE_LAYERS_FACTOR = 3.0
+
 # Int32 values are in fixed point form with gram and millimeter precision
 # respectively.
 weight_dtype = np.dtype([('layer', np.uint8), ('weight', np.int32), ('pos', np.int32)])
+
+def fitness(indiv):
+    fit = 0.0
+    # If the layer is greater than 7 we need to penalize it somehow. We need
+    # one weight per layer for 7 layers total.
+    for weight in indiv:
+        fit += max(0, weight['layer'] - 7) * TOO_MANY_LAYERS_FACTOR
+
+    # We can't have duplicate layers
+    layers = [weight['layer'] for weight in indiv]
+    for i in range(WEIGHTS_PER_MOBILE):
+        fit += max(0, layers.count(i) - 1) * DUPLICATE_LAYERS_FACTOR
+
+    return fit
 
 if __name__ == '__main__':
     # Create the initial species
@@ -14,4 +31,12 @@ if __name__ == '__main__':
                                   WEIGHTS_PER_MOBILE)
     species = np.fromstring(rand_source, weight_dtype)
     species.shape = (INITIAL_SPECIES_SIZE, WEIGHTS_PER_MOBILE)
-    print(species[0])
+
+    species_fitness = []
+    for indiv in species:
+        species_fitness.append((fitness(indiv), indiv))
+
+    # Sort by fitness value
+    species_fitness = sorted(species_fitness, key=lambda val: val[0])
+
+    print(species_fitness)
